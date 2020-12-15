08c875ada3ed65eb7ad9ab31e6e5bae4437545e9
dblur <dblur@ya.ru>
2020-12-15 19:10:21 +0700

    create SpringEdition

--- src/main/java/app/DAO/DaoConnection.java
+++
@@ -1,9 +0,0 @@
-package app.DAO;
-
-import app.database.DatabaseConnection;
-
-public class DaoConnection {
-    public static UserDAO getDaoConnection() {
-        return new UserJdbcDAO(DatabaseConnection.getSQLConnection());
-    }
-}
